9154a01cd474ec57c01e40285aa7a70b962c7f2b
chauhanvinay089 <ramvinaychauhan041@gmail.com>
2024-07-22 17:39:00 +0530

    conflict sol e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,56 +13,5 @@ export class AppComponent {
    this. isShwow=true
   }
 
-  public feed: Observable<Message[]>;
-
-  public readonly user: User = {
-    id: 1
-  };
-
-  public readonly bot: User = {
-    id: 0
-  };
-
-  private local: Subject<Message> = new Subject<Message>();
-
-  constructor(private svc: ChatService) {
-    const hello: Message = {
-      author: this.bot,
-      suggestedActions: [{
-        type: 'reply',
-        value: 'Request Notes'
-      }, {
-        type: 'reply',
-        value: 'Raise a Query'
-      }],
-      timestamp: new Date(),
-      text: 'Hey, This is AdGo Virtual Underwriter. How can I help you today ?'
-    };
-
-    // Merge local and remote messages into a single stream
-    this.feed = merge(
-      from([ hello ]),
-      this.local,
-      this.svc.responses.pipe(
-        map((response:string): Message => ({
-          author: this.bot,
-          text: response
-        })
-      ))
-    ).pipe(
-      // ... and emit an array of all messages
-      scan((acc: Message[], x: Message) => [...acc, x], [])
-    );
-  }
-
-  public sendMessage(e: SendMessageEvent): void {
-    this.local.next(e.message);
-
-    this.local.next({
-      author: this.bot,
-      typing: true
-    });
-
-    this.svc.submit(e.message.text);
-  }
+  
 }
