ae9b2790ca557253b2b24a03a142bcbd06c2ec12
chauhanvinay089 <ramvinaychauhan041@gmail.com>
2024-07-22 17:33:34 +0530

    add File in html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,58 @@ export class AppComponent {
   isShwow:boolean=false
   add(){
    this. isShwow=true
+  }
+
+  public feed: Observable<Message[]>;
+
+  public readonly user: User = {
+    id: 1
+  };
+
+  public readonly bot: User = {
+    id: 0
+  };
+
+  private local: Subject<Message> = new Subject<Message>();
+
+  constructor(private svc: ChatService) {
+    const hello: Message = {
+      author: this.bot,
+      suggestedActions: [{
+        type: 'reply',
+        value: 'Request Notes'
+      }, {
+        type: 'reply',
+        value: 'Raise a Query'
+      }],
+      timestamp: new Date(),
+      text: 'Hey, This is AdGo Virtual Underwriter. How can I help you today ?'
+    };
+
+    // Merge local and remote messages into a single stream
+    this.feed = merge(
+      from([ hello ]),
+      this.local,
+      this.svc.responses.pipe(
+        map((response:string): Message => ({
+          author: this.bot,
+          text: response
+        })
+      ))
+    ).pipe(
+      // ... and emit an array of all messages
+      scan((acc: Message[], x: Message) => [...acc, x], [])
+    );
+  }
+
+  public sendMessage(e: SendMessageEvent): void {
+    this.local.next(e.message);
+
+    this.local.next({
+      author: this.bot,
+      typing: true
+    });
+
+    this.svc.submit(e.message.text);
   }
 }
